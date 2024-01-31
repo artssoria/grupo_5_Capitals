d2e694951b8b76fed558512406bd36e39390c98f
artssoria <53129227+artssoria@users.noreply.github.com>
2024-01-31 17:17:50 -0300

    enlace del registro con los datos

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ app.get('/register', function(req, res) {
     res.sendFile(path.join(__dirname, 'views', 'register.html'));
 });
 
+// Middleware para analizar el cuerpo de las solicitudes POST
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/register', (req, res) => {
+    // Manejar los datos del formulario
+    console.log(req.body);
+
+    // Redirige al usuario a home.html después de enviar el formulario
+    res.redirect('/'); // Redirige a la página de inicio
+});
+
 
 app.listen(3000, function() {
     console.log('Arrancando Express desde el puerto 3000');
