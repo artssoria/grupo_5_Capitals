63904d1a354461e3c2646b409cb79b5409e03944
Diniar90 <arjonadiego90@gmail.com>
2024-02-05 20:20:56 -0300

    carrito Facundo

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'home.html')); // 
+    res.sendFile(path.join(__dirname,'views', 'home.html')); // 
 });
 // Ruta para la páginas descubri (4 html)
 app.get('/puna', function(req, res) {
