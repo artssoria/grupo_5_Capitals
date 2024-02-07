769f0c6cbc8820b68b274000dafda3b8a63c9177
Diniar90 <arjonadiego90@gmail.com>
2024-02-06 23:46:53 -0300

    admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -3,6 +3,14 @@ const path = require('path');
 const adminController = {
     carga: (req,res) =>{
         res.render('carga-productos')
+    },
+
+    modif: (req,res) => {
+        res.render('modif-productos')
+    },
+
+    panel: (req,res) => {
+        res.render('panel')
     }
 };
 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,4 +5,8 @@ const adminController = require('../controllers/adminController');
 
 router.get('/carga-producto', adminController.carga);
 
+router.get('/modif-producto', adminController.modif);
+
+router.get('/panel', adminController.panel);
+
 module.exports = router;
