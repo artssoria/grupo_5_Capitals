b0c1989ea25e9d5fb79f8307c70564d8564019d2
Diniar90 <arjonadiego90@gmail.com>
2024-03-04 04:02:07 -0300

    admin post crear producto

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,13 +1,29 @@
 const path = require('path');
 const fs = require('fs');
+const filePath = path.join(__dirname, '../data/products.json');
+const crypto = require('crypto');
+const products = require('../data/products.json');
 
 const adminController = {
     carga: (req,res) =>{
         res.render('carga-productos')
     },
 
+    subir: (req,res) =>{
+        let newProduct = req.body;
+
+        newProduct.id = `${products.length + 1}`;
+
+        newProduct.imagen_product = "/images/" + req.file?.filename || "default-image.png";
+
+        products.push(newProduct);
+
+        fs.writeFileSync( filePath, JSON.stringify(products, null, 2));
+
+        res.redirect('/admin/modif-producto');
+    },
+
     listado: (req,res) => {
-        const filePath = path.join(__dirname, '../data/products.json');
         let obtenerProductos = fs.readFileSync(filePath, 'utf-8');
         let productos = JSON.parse(obtenerProductos);
         res.render('modif-productos', {listadoDeProductos: productos});
@@ -19,13 +35,39 @@ const adminController = {
     },
 
     edit: (req,res) =>{
-        const filePath = path.join(__dirname, '../data/products.json');
         let obtenerProductos = fs.readFileSync(filePath, 'utf-8');
         let productos = JSON.parse(obtenerProductos);
         let idProducto = (req.params.idProducto)- 1;
         let productoToEdit = productos[idProducto];
         res.render("productEdit", {'productoToEdit': productoToEdit});
-    } 
+    },
+
+    actualizar: (req,res)=>{
+        const { id, nuevaInfo } = req.body;
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+              return res.status(500).send('Error al leer el archivo');
+            }
+      
+            let products = JSON.parse(data);
+
+            const productIndex = products.findIndex(product => product.id === id);
+
+            if (productIndex === -1) {
+                return res.status(404).send('Producto no encontrado');
+            };
+
+            products[productIndex] = { ...products[productIndex], ...nuevaInfo };
+
+            fs.writeFile(filePath, JSON.stringify(products, null, 2), err => {
+                if (err) {
+                  console.error(err);
+                  return res.status(500).send('Error al escribir en el archivo');
+                };
+            });
+        });
+    }
 };
 
 module.exports = adminController;
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,7 +5,21 @@ const fs = require('fs');
 const multer = require('multer');
 const adminController = require('../controllers/adminController');
 
+const storage = multer.diskStorage({
+    destination: (req,file,cb) =>{
+        cb(null, path.join(__dirname, '../../public/images'));
+    },
+    filename: (req,file,cb) =>{
+        const newFilename = 'imagen-'+ Date.now() + path.extname(file.originalname);
+        cb(null, newFilename);
+    }
+});
+
+const upload = multer({storage});
+
 router.get('/carga-producto', adminController.carga);
+
+router.post('/carga-productos', upload.single('imagen_product'), adminController.subir);
 
 router.get('/modif-producto', adminController.listado);
 
@@ -13,9 +27,7 @@ router.get('/panel', adminController.panel);
 
 router.get('/edit/:idProducto', adminController.edit);
 
-router.put('/edit', (req,res) =>{
-    res.send('Fui por PUT');
-});
+router.put('/edit', adminController.actualizar);
 
 router.delete('/delete/:idProducto', (req,res) =>{
     res.send('FUI por DELETE');
